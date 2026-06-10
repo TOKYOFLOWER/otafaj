4b8adf7e961cfdf5eccd23a6b87508a7c993f0db
TOKYOFLOWER <tokyoflowercoltd+github@gmail.com>
2026-06-11 01:23:39 +0900

    Add files via upload

---
+++ docs/config.js
@@ -0,0 +1,8 @@
+// 接続設定(このファイルだけ編集すればOK。画面には表示されません)
+window.LOSS_CONFIG = {
+  // GASウェブアプリのURL(デプロイで取得した /exec のURL)
+  gasUrl: 'https://script.google.com/macros/s/ここにデプロイIDを貼る/exec',
+
+  // 何日前まで仕入データをさかのぼるか(初期値: 半月)
+  days: 15
+};
