34a6b9d587830ba43252bba4544c39823f3a2599
TOKYOFLOWER <tokyoflowercoltd+github@gmail.com>
2026-06-11 01:31:12 +0900

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,7 +1,7 @@
 // 接続設定(このファイルだけ編集すればOK。画面には表示されません)
 window.LOSS_CONFIG = {
   // GASウェブアプリのURL(デプロイで取得した /exec のURL)
-  gasUrl: 'https://script.google.com/macros/s/ここにデプロイIDを貼る/exec',
+  gasUrl: 'https://script.google.com/macros/s/AKfycbwQNIY2zx6GAGhLSgOP9eGSnW-FCXNj0hoY3deFfnOydW2BRL52HX0v5Xlrk7iun8fT/exec',
 
   // 何日前まで仕入データをさかのぼるか(初期値: 半月)
   days: 15
